f8cafaf94f6c488205d8dbf5178665cfc65b2627
Krzysztof Burghardt <krzysztof@burghardt.pl>
2023-02-14 00:09:39 +0100

    Relicensed to GPL v3.0 or later.

--- src/kolonia.h
+++ src/kolonia.h
@@ -4,9 +4,9 @@
    This file is part of Wampirzce.
    Copyright (C) 2004 Krzysztof Burghardt.
 
-   This program is free software; you can redistribute it and/or modify
+   This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2 of the License, or
+   the Free Software Foundation, either version 3 of the License, or
    (at your option) any later version.
 
    This program is distributed in the hope that it will be useful,
@@ -15,11 +15,8 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
-
-// $Id: kolonia.h,v 1.6 2007-11-01 17:07:20 kb Exp $
 
 #ifndef __KOLONIA_H__
 #define __KOLONIA_H__
--- src/polozenie.C
+++ src/polozenie.C
@@ -4,9 +4,9 @@
    This file is part of Wampirzce.
    Copyright (C) 2004 Krzysztof Burghardt.
 
-   This program is free software; you can redistribute it and/or modify
+   This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2 of the License, or
+   the Free Software Foundation, either version 3 of the License, or
    (at your option) any later version.
 
    This program is distributed in the hope that it will be useful,
@@ -15,11 +15,8 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
-
-// $Id: polozenie.C,v 1.2 2004-02-05 00:14:38 einstein Exp $
 
 #include <iostream>
 #include "polozenie.h"
